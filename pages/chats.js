29fb9f92058d76ca670b58b5a9e56b9393df9ef0
anomius <swarajpurohit@gmail.com>
2022-01-27 01:46:29 +0530

    lol

--- pages/chats.js
+++ pages/chats.js
@@ -6,42 +6,44 @@ import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
 const ChatEngine = dynamic(() =>
-  import("react-chat-engine").then((module) => module.ChatEngine)
+    import ("react-chat-engine").then((module) => module.ChatEngine)
 );
 const MessageFormSocial = dynamic(() =>
-  import("react-chat-engine").then((module) => module.MessageFormSocial)
+    import ("react-chat-engine").then((module) => module.MessageFormSocial)
 );
 
 export default function Home() {
-  const { username, secret } = useContext(Context);
-  const [showChat, setShowChat] = useState(false);
-  const router = useRouter();
+    const { username, secret } = useContext(Context);
+    const [showChat, setShowChat] = useState(false);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (typeof document !== undefined) {
-      setShowChat(true);
-    }
-  }, []);
+    useEffect(() => {
+        if (typeof document !== undefined) {
+            setShowChat(true);
+        }
+    }, []);
 
-  useEffect(() => {
-    if (username === "" || secret === "") {
-      router.push("/");
-    }
-  }, [username, secret]);
+    useEffect(() => {
+        if (username === "" || secret === "") {
+            router.push("/");
+        }
+    }, [username, secret]);
 
-  if (!showChat) return <div />;
+    if (!showChat) return <div / > ;
 
-  return (
-    <div className="background">
-      <div className="shadow">
-        <ChatEngine
-          height="calc(100vh - 212px)"
-          projectID="b60a6d8b-d377-477e-af88-e47de35b3e89"
-          userName={username}
-          userSecret={secret}
-          renderNewMessageForm={() => <MessageFormSocial />}
-        />
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "background" >
+        <
+        div className = "shadow" >
+        <
+        ChatEngine height = "calc(100vh - 212px)"
+        projectID = "f4500983-db76-4fa3-a3c8-7f5b3e79b56f"
+        userName = { username }
+        userSecret = { secret }
+        renderNewMessageForm = {
+            () => < MessageFormSocial / > }
+        /> <
+        /div> <
+        /div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,57 +7,66 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 const Auth = () => {
-  const { username, setUsername, secret, setSecret } = useContext(Context);
+    const { username, setUsername, secret, setSecret } = useContext(Context);
 
-  const router = useRouter();
+    const router = useRouter();
 
-  function onSubmit(e) {
-    e.preventDefault();
+    function onSubmit(e) {
+        e.preventDefault();
 
-    if (username.length === 1 || secret.length === 1) return;
+        if (username.length === 1 || secret.length === 1) return;
 
-    axios
-      .put(
-        "https://api.chatengine.io/users/",
-        { username, secret },
-        { headers: { "Private-Key": "c2f82e63-9978-4c5c-9c17-8b0dec845dc6" } }
-      )
+        axios
+            .put(
+                "https://api.chatengine.io/users/", { username, secret }, { headers: { "Private-Key": "bf20292d-0ed3-4f54-93ab-c3171def5b87" } }
+            )
 
-      .then((r) => {
-        router.push("/chats");
-      });
-  }
+        .then((r) => {
+            router.push("/chats");
+        });
+    }
 
-  return (
-    <div className="background">
-      <div className="auth-container">
-        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
-          <div className="auth-title">NextJS Chat</div>
+    return ( <
+        div className = "background" >
+        <
+        div className = "auth-container" >
+        <
+        form className = "auth-form"
+        onSubmit = {
+            (e) => onSubmit(e) } >
+        <
+        div className = "auth-title" > NextJS Chat < /div>
 
-          <div className="input-container">
-            <input
-              placeholder="Email"
-              className="text-input"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
+        <
+        div className = "input-container" >
+        <
+        input placeholder = "Email"
+        className = "text-input"
+        onChange = {
+            (e) => setUsername(e.target.value) }
+        /> <
+        /div>
 
-          <div className="input-container">
-            <input
-              type="password"
-              placeholder="Password"
-              className="text-input"
-              onChange={(e) => setSecret(e.target.value)}
-            />
-          </div>
+        <
+        div className = "input-container" >
+        <
+        input type = "password"
+        placeholder = "Password"
+        className = "text-input"
+        onChange = {
+            (e) => setSecret(e.target.value) }
+        /> <
+        /div>
 
-          <button type="submit" className="submit-button">
-            Login / Sign Up
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+        <
+        button type = "submit"
+        className = "submit-button" >
+        Login / Sign Up <
+        /button> <
+        /form> <
+        /div> <
+        /div>
+    );
 };
 
 export default Auth;
